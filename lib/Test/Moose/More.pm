d1201fb1f3d791231d019ddc12aea207d7d435bc
Chris Weyl <cweyl@alumni.drew.edu>
2012-01-18 12:08:32 -0800

    initial commit

---
+++ lib/Test/Moose/More.pm
@@ -0,0 +1,38 @@
+package Test::Moose::More;
+
+# ABSTRACT: The great new Test::Moose::More!
+
+use Moose;
+use namespace::autoclean;
+use common::sense;
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+L<Smart::Comments>, L<Test::NoSmartComments>
+
+=head1 BUGS
+
+All complex software has bugs lurking in it, and this module is no exception.
+
+Please report any bugs to
+"bug-Test::Moose::More@rt.cpan.org",
+or through the web interface at <http://rt.cpan.org>.
+
+Bugs, feature requests and pull requests through GitHub are most welcome; our
+page and repo (same URI):
+
+    https://github.com/RsrchBoy/test::moose::more
+
+=cut
+
