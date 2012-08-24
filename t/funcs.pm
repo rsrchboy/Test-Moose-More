71d8bb8f0679aa9f7dde76d6e696ec39dd2c489f
Chris Weyl <cweyl@alumni.drew.edu>
2012-08-24 14:55:45 -0700

    Add $_skip to counters()

--- t/funcs.pm
+++ t/funcs.pm
@@ -1,12 +1,13 @@
 
 # use as:
-my ($_ok, $_nok) = counters();
+my ($_ok, $_nok, $_skip) = counters();
 
 sub counters {
     my $i = 1;
     return (
         sub {     'ok ' . $i++ . " - $_[0]" },
         sub { 'not ok ' . $i++ . " - $_[0]" },
+        sub {     'ok ' . $i++ . " # skip $_[0]" },
     );
 }
 
