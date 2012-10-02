a34d489c0313037ec87c5e19f4a263b0f76f97e6
Chris Weyl <cweyl@alumni.drew.edu>
2012-10-02 00:03:27 -0700

    t/funcs: add plan sub

--- t/funcs.pm
+++ t/funcs.pm
@@ -3,11 +3,16 @@
 my ($_ok, $_nok, $_skip) = counters();
 
 sub counters {
-    my $i = 1;
+    my $level = shift @_ || 0;
+    my $i = 0;
+
+    my $indent = !$i ? q{} : $i x ' ';
+
     return (
-        sub {     'ok ' . $i++ . " - $_[0]" },
-        sub { 'not ok ' . $i++ . " - $_[0]" },
+        sub {     'ok ' . $i++ . " - $_[0]"      },
+        sub { 'not ok ' . $i++ . " - $_[0]"      },
         sub {     'ok ' . $i++ . " # skip $_[0]" },
+        sub { "1..$i"                            },
     );
 }
 
