2b926508750455ecaa1d38b4d5d5dea7a2338cf8
Chris Weyl <cweyl@alumni.drew.edu>
2016-11-10 22:52:47 -0600

    initial add: Test::Moose::More::Utils
    I'm finding that there are a couple utility functions I've created
    (liberated/stolen/etc) that are useful for TMM, but may have utility
    beyond TMM.  To facilitate that, we split them into their own package.

---
+++ lib/Test/Moose/More/Utils.pm
@@ -0,0 +1,56 @@
+package Test::Moose::More::Utils;
+
+# ABSTRACT: Various utility functions for TMM (and maybe others!)
+
+use strict;
+use warnings;
+
+use Sub::Exporter::Progressive -setup => {
+
+    exports => [ qw{
+        get_mop_metaclass_for
+    } ],
+
+    groups  => { default  => [':all'] },
+};
+
+use List::Util 1.33 qw( first all );
+use Scalar::Util 'blessed';
+
+=func get_mop_metaclass_for $mop => $meta
+
+Given a MOP name (e.g. attribute), rummage through $meta (a metaclass) to reveal the MOP's metaclass.
+
+e.g.
+
+    get_metaclass_for attribute => __PACKAGE__->meta;
+
+=cut
+
+sub get_mop_metaclass_for {
+    my ($mop, $meta) = @_;
+
+    # FIXME make this less... bad
+    # short-circuit!  Better a special case here than *everywhere* else
+    return blessed $meta
+        if $mop eq 'class' || $mop eq 'role';
+
+    # this code largely lifted from Moose::Util::MetaRole
+    my $attr =
+        first { $_ }
+        map { $meta->meta->find_attribute_by_name($_) }
+        ("${mop}_metaclass", "${mop}_class")
+        ;
+    my $read_method = $attr->get_read_method;
+
+    return $meta->$read_method();
+}
+
+!!42;
+__END__
+
+=head1 SEE ALSO
+
+L<Moose::Util::MetaRole> -- for much of the "find the metaclass for X mop" code
+
+=cut
